70d5794bd866f9d49a854687c5aa62dab24e01ea
plesoczkiadam <43034243+plesoczkiadam@users.noreply.github.com>
2021-11-11 17:49:41 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,42 +4,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace ConsoleApp2
 {
     class Program
     {
+        private const int n =30;
+        static int[] a = new int[n];
+
+        static void generalas()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = rnd.Next(-50, 51);
+            }
+        }
+        static void kiir()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+        }
+        static void beillesztrendez()
+        {
+            for (int i = 1; i < n; i++)
+            {
+                int j = i - 1;
+                int x = a[i];
+                while ((j > -1) && (x < a[j]))
+                {
+                    a[j + 1] = a[j];
+
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            int i, j;
-            bool v;
-            v = true;
-            for (i = 1; i < 9; i++)
-            {
-                if (i % 2 == 1)
-                {
-                    v = true;
-
-                }
-                else
-                {
-                    v = false;
-                }
-            }
-            for (j = 1; j < 9; i++)
-            {
-                if (v)
-                {
-                    Console.WriteLine("A");
-                }
-                else 
-                {
-                    Console.WriteLine("B");
-                }
-            }
-            v = (v);
-            Console.WriteLine("");
+            generalas();
+            kiir();
+            beillesztrendez();
+            kiir();
             Console.ReadLine();
-
         }
     }
 }
