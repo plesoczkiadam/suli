34fc9ecf4cf76b7a192fb9c0d94e2fd4dee5633e
plesoczkiadam <43034243+plesoczkiadam@users.noreply.github.com>
2021-11-11 17:47:24 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            bool v;
+            v = true;
+            for (i = 1; i < 9; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    v = true;
+
+                }
+                else
+                {
+                    v = false;
+                }
+            }
+            for (j = 1; j < 9; i++)
+            {
+                if (v)
+                {
+                    Console.WriteLine("A");
+                }
+                else 
+                {
+                    Console.WriteLine("B");
+                }
+            }
+            v = (v);
+            Console.WriteLine("");
+            Console.ReadLine();
+
+        }
+    }
+}
