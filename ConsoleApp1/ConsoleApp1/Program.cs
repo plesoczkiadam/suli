2d9bdf1c535c2344474291c99ebe79ce27e6605d
plesoczkiadam <43034243+plesoczkiadam@users.noreply.github.com>
2022-04-11 11:26:27 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("enaplo.txt", FileMode.Open);
+            StreamReader rs = new StreamReader(fs);
+            string s = rs.ReadLine();
+            while (s != null)
+            {
+                Console.WriteLine(s);
+                s = rs.ReadLine();
+
+            }
+            rs.Close();
+            fs.Close();
+            Console.ReadKey();
+        }
+    }
+}
